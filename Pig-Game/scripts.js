e26f49f49eb1c71a98bca2dedcfc8e0e5b98df55
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-01-10 12:16:19 +0200

    on click of btn roll

--- Pig-Game/scripts.js
+++ Pig-Game/scripts.js
@@ -2,3 +2,5 @@
 var scores, roundScores, activePlayer, gamePlaying, lastDice;
 // call intialised function
 init();
+// on click of btn roll
+document.querySelector('.btn-roll').addEventListener('click', function() {
